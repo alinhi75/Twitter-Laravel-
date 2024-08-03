22b97318b0cda16186c8c9958fb24cbc02a37a4a
alinhi75 <sayedali.noohi@studenti.polito.it>
2024-08-03 13:46:55 +0200

    Layout&DBConnection

---
+++ twitter/resources/views/terms.blade.php
@@ -0,0 +1,7 @@
+@extends ('layout.layout')
+
+@section ('content')
+<h1>terms and conditions</h1>
+<p>These are the terms and conditions</p>
+@endsection
+
--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -2,6 +2,54 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+
+/*
+
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+
+*/
+
+
+// feed
+
+
+// profile
+
+
+// auth
+
+
+// home
+
+/*
+
+MVC
+
+Model: Database
+
+View: User Interface
+
+Controller: Logic
+
+
+*/
+
+
+
+
+Route::get('/', [DashboardController::class , 'index']);
+
+
+// Route::get('/profile', [ProfileController::class , 'index']);
+
+Route::get('/terms', function() {
+    return view('terms');
 });
