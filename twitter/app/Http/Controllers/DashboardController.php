ed1393247ac9b6adb8ae5a99b1c53d464c1d8afa
alinhi75 <sayedali.noohi@studenti.polito.it>
2024-08-03 15:19:15 +0200

    ConfigureModel

--- twitter/app/Http/Controllers/DashboardController.php
+++ twitter/app/Http/Controllers/DashboardController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
+use Illuminate\Http\Request;
+use App\Models\Idea;
 class DashboardController extends Controller
 {
     public function index() {
-        return view('dashboard');
+
+        $idea = new Idea([
+            'content' => 'Hello this is my first idea',
+            'likes' => 0
+        ]);
+        $idea->save();
+
+        dump(Idea::all());
+        return view('dashboard',[
+            'ideas' => Idea::orderBy('created_at','DESC')->get()
+        ]);
     }
 }
 
